876fe5573f8e2b92ccf1232b50c55a6a3bbe44e5
Oleh Marchuk <olehmarchukdev@gmail.com>
2023-11-15 15:35:31 +0200

    Add OfferDraft type for session flavor

--- src/types/index.ts
+++ src/types/index.ts
@@ -9,10 +9,19 @@ export type PretikConversation = Conversation<PretikContext>
 
 interface SessionData {
   employeeData: employeeData
+  offerDraft: OfferDraft
 }
 
 interface employeeData {
   employeeId: string
   employeeName: string
   employeePhoneNumber: string
+}
+
+interface OfferDraft {
+  shortName: string
+  reasons: string
+  content: string
+  solvesProblem: string
+  responsibleDepartment: string
 }
