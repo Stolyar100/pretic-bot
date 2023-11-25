b651649b6721324f1fac831924863cdaf8f4ca98
Oleh Marchuk <olehmarchukdev@gmail.com>
2023-11-25 23:44:16 +0200

    Separete error handler from index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,23 +6,11 @@ import { MainMenuModule } from './modules/main-menu/main-menu-module.js'
 import { AuthModule } from './modules/auth/auth-module.js'
 import { PretikContext } from './types/index.js'
 import { OfferModule } from './modules/offer/offer-module.js'
+import { errorHandler } from './helpers/errorHandler.js'
 
 const { BOT_TOKEN } = env
 
 const pretikBot = new Bot<PretikContext>(BOT_TOKEN)
-
-const errorHandler = (err: BotError) => {
-  const ctx = err.ctx
-  console.error(`Error while handling update ${ctx.update.update_id}:`)
-  const e = err.error
-  if (e instanceof GrammyError) {
-    console.error('Error in request:', e.description)
-  } else if (e instanceof HttpError) {
-    console.error('Could not contact Telegram:', e)
-  } else {
-    console.error('Unknown error:', e)
-  }
-}
 
 pretikBot.catch((err: BotError) => errorHandler(err))
 
