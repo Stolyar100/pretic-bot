e3d16013e0e5d7bcc3fb79f626fda7d4399a8e69
Oleh Marchuk <olehmarchukdev@gmail.com>
2023-12-16 20:12:01 +0200

    Update .env schema

--- src/config/schema.ts
+++ src/config/schema.ts
@@ -1,7 +1,8 @@
 const schema = {
   BOT_TOKEN: String,
   DATABASE_URL: String,
-  CHANNEL_URL: String,
+  CHANNEL_INVITE_URL: String,
+  CHANNEL_ID: Number,
   ADMIN_TAB_NUMBER: String,
 } as const
 
