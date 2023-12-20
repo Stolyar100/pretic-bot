baff66a50442e04646bdb242b8fbc6b490c6abd3
Oleh Marchuk <olehmarchukdev@gmail.com>
2023-12-20 12:41:11 +0200

    Squash migration

--- src/prisma/migrations/20231218203427_add_offer_cascade_delete/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "TGBotOffer" DROP CONSTRAINT "TGBotOffer_AuthorId_fkey";
-
--- AddForeignKey
-ALTER TABLE "TGBotOffer" ADD CONSTRAINT "TGBotOffer_AuthorId_fkey" FOREIGN KEY ("AuthorId") REFERENCES "TGBotUser"("TelegramId") ON DELETE CASCADE ON UPDATE CASCADE;
---
+++ src/prisma/migrations/20231220103334_squashed_migrations/migration.sql
@@ -0,0 +1,39 @@
+-- CreateEnum
+CREATE TYPE "TGBotOfferStatus" AS ENUM ('PENDING', 'ACCEPTED', 'REJECTED');
+
+-- CreateTable
+CREATE TABLE "TGBotEmployee" (
+    "TabNumber" VARCHAR(4) NOT NULL,
+    "FullName" TEXT NOT NULL,
+    "Phone" TEXT,
+
+    CONSTRAINT "TGBotEmployee_pkey" PRIMARY KEY ("TabNumber")
+);
+
+-- CreateTable
+CREATE TABLE "TGBotUser" (
+    "TelegramId" INTEGER NOT NULL,
+    "EmployeeTabNumber" VARCHAR(4) NOT NULL,
+
+    CONSTRAINT "TGBotUser_pkey" PRIMARY KEY ("TelegramId")
+);
+
+-- CreateTable
+CREATE TABLE "TGBotOffer" (
+    "Id" SERIAL NOT NULL,
+    "ShortName" VARCHAR(70) NOT NULL,
+    "Status" "TGBotOfferStatus" NOT NULL DEFAULT 'PENDING',
+    "AuthorId" INTEGER NOT NULL,
+    "CreationDate" DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "TGBotOffer_pkey" PRIMARY KEY ("Id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "TGBotUser_EmployeeTabNumber_key" ON "TGBotUser"("EmployeeTabNumber");
+
+-- AddForeignKey
+ALTER TABLE "TGBotUser" ADD CONSTRAINT "TGBotUser_EmployeeTabNumber_fkey" FOREIGN KEY ("EmployeeTabNumber") REFERENCES "TGBotEmployee"("TabNumber") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TGBotOffer" ADD CONSTRAINT "TGBotOffer_AuthorId_fkey" FOREIGN KEY ("AuthorId") REFERENCES "TGBotUser"("TelegramId") ON DELETE CASCADE ON UPDATE CASCADE;
